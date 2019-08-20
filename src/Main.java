ffc7ff2759bf9fa68329c6471a3976c0c14ae7d5
DamnVini <vinicius_v_jesus@estudante.sc.senai.br>
2019-08-19 21:17:05 -0300

    alterei os numeros na saida de info

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 			
 			
 			System.out.println("O nome da pessoa1 é " + pessoa1.getNome() + ", ela tem " + pessoa1.getIdade() + " anos e é do sexo: " + pessoa1.getSexo() + ". " + "Ela mora no bairro: " + endereco1.getBairro() + ", na rua: " + endereco1.getRua() + " N#" + endereco1.getNumero());
-			System.out.println("O nome da pessoa1 é " + pessoa2.getNome() + ", ela tem " + pessoa2.getIdade() + " anos e é do sexo: " + pessoa2.getSexo() + ". " + "Ela mora no bairro: " + endereco2.getBairro() + ", na rua: " + endereco2.getRua() + " N#" + endereco2.getNumero());
-			System.out.println("O nome da pessoa1 é " + pessoa3.getNome() + ", ela tem " + pessoa3.getIdade() + " anos e é do sexo: " + pessoa3.getSexo() + ". " + "Ela mora no bairro: " + endereco3.getBairro() + ", na rua: " + endereco3.getRua() + " N#" + endereco3.getNumero());
+			System.out.println("O nome da pessoa2 é " + pessoa2.getNome() + ", ela tem " + pessoa2.getIdade() + " anos e é do sexo: " + pessoa2.getSexo() + ". " + "Ela mora no bairro: " + endereco2.getBairro() + ", na rua: " + endereco2.getRua() + " N#" + endereco2.getNumero());
+			System.out.println("O nome da pessoa3 é " + pessoa3.getNome() + ", ela tem " + pessoa3.getIdade() + " anos e é do sexo: " + pessoa3.getSexo() + ". " + "Ela mora no bairro: " + endereco3.getBairro() + ", na rua: " + endereco3.getRua() + " N#" + endereco3.getNumero());
 			}	
 	}
 
